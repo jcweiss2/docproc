96601a5ab070bdf37568b79635cb4a42dbc1cc85
Jeremy C. Weiss <jeremyweiss@cmu.edu>
2018-04-16 18:30:16 -0400

    separate h->c updates and h->o optimizers

--- jcw_cluster3.py
+++ jcw_cluster3.py
@@ -40,12 +40,12 @@ max_block_height = 100
 
 subsample_stride = 1
 
-o_learning_rate = 1e-3  # 2e-4
+o_learning_rate = 1e-4  # 2e-4
 g_learning_rate = 1e-5
 c_learning_rate = 1e-3
 # optim_betas = (0.9, 0.999)
-num_epochs = 1000000
-print_interval = 4
+num_epochs = 100
+print_interval = 1
 image_interval = 16
 
 # anchor_only=False
@@ -56,10 +56,6 @@ suffix = ''
 
 # gpu-mode
 gpu_mode = True
-
-if gpu_mode:
-    t = Variable(torch.FloatTensor(np.array([0,0.5,1]))).cuda()
-    ts = t + t
     
 ### My data not theirs.
 # mydata = torch.Tensor(np.zeros((data_size,g_output_size)))
@@ -72,26 +68,25 @@ if gpu_mode:
 #     mydata[xlb:xub,ylb:yub] = torch.Tensor(np.random.poisson(np.random.randint(10), size=(xub-xlb, yub-ylb)))
 # mydata = mydata.clone()
 
-# mydatadf = pd.read_csv('medicare/PartD_Prescriber_PUF_NPI_Drug_15_total_claims_wide.csv')
-# mydata = torch.log(torch.FloatTensor(mydatadf.drop(['npi'], axis=1).as_matrix()+1))
+mydatadf = pd.read_csv('medicare/PartD_Prescriber_PUF_NPI_Drug_15_total_claims_wide.csv')
+mydata = Variable(torch.log(torch.FloatTensor(mydatadf.drop(['npi'], axis=1).as_matrix()+1)))
 
-# mydata = mydata.as_matrix()
-# mydata = mydata * mydata.shape[0] / np.expand_dims((mydata>0).sum(0),0)  # try tf/idf
-# mydata = np.log(1+mydata)
-# mydata = mydata.transpose() # comment out if you want to cluster over physicians instead.
+# mydata = mydata.t() # comment out if you want to cluster over physicians instead.
 
-clustercsv = 'medicare/small_wide.csv'
-mypd = pd.read_csv(clustercsv)
-mypd = mypd.drop(columns='npi')
-mydata = Variable(torch.log(torch.FloatTensor(mypd.as_matrix())+1))
+# clustercsv = 'medicare/small_wide.csv'
+# mypd = pd.read_csv(clustercsv)
+# mypd = mypd.drop(columns='npi')
+# mydata = Variable(torch.log(torch.FloatTensor(mypd.as_matrix())+1))
 
 data_size = mydata.size()[0]
-g_input_size = 256  # noise input size
-hidden_size = 256  # latent space size
+g_input_size = 1024  # noise input size
+hidden_size = 128  # latent space size
 g_output_size = hidden_size
 c_input_size = hidden_size
-c_output_size = 50  # # clusters
+c_hidden_size = 32
+c_output_size = 100  # # clusters
 o_input_size = hidden_size
+o_hidden_size = hidden_size
 o_output_size = mydata.size()[1]
 
 # ### Uncomment only one of these
@@ -137,10 +132,10 @@ def subsample(matrix, subsample_stride=1, flip_long=True):
 
 # ##### MODELS: Generator model and discriminator model
 class Generator(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, hd):
         super(Generator, self).__init__()
         self.map1 = nn.Linear(input_size, hidden_size)
-        # self.hd = hd[int(np.log2(hidden_size))-1]
+        self.hd = hd[int(np.log2(hidden_size))-1].t()  # value (input) to freq (output) space, to pool
         self.permute_number = 4
         self.permuteTensor = torch.cat(tuple([SignMatrix(hidden_size).matmul(MakePermuteMatrix(hidden_size)).unsqueeze(0) for p in range(self.permute_number)]),0)  # PN x H x H
         self.batchnorm1 = nn.BatchNorm1d(hidden_size)
@@ -152,39 +147,57 @@ class Generator(nn.Module):
         self.pool = nn.MaxPool1d(3,stride=2)
 
     def forward(self, x):
-        x = F.elu(self.map1(x))
-        # x = self.batchnorm1(x.matmul(self.hd))
+        x = F.tanh(self.map1(x))
         xs = x.matmul(self.permuteTensor)  # PN x B x H
         # pdb.set_trace()
         # x = torch.max(x,2)[0]
-        x = xs.permute((1,2,0))
+        x = x.matmul(self.hd)  # x.matmul(self.hd)
+        x = xs.permute((1,2,0))  # B x H x PN
         x = self.batchnorm2(self.pool(x)).sum(2)
         x = F.leaky_relu(self.map2(x))
         x = self.batchnorm3(self.map3(x))
-        return F.leaky_relu(self.map4(F.elu(x)),1e-2)
+        return F.leaky_relu(self.map4(F.tanh(x)),1e-1)
 
     def lastlayer(self, x):
-        return F.leaky_relu(self.map4(F.elu(x)),1e-2)
+        return F.leaky_relu(self.map4(F.tanh(x)),1e-1)
 
     
 class Clusterer(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size):
         super(Clusterer, self).__init__()
-        self.map1 = nn.Linear(input_size, output_size)
+        self.map1 = nn.Linear(input_size, hidden_size)
+        self.map2 = nn.Linear(hidden_size, hidden_size)
+        self.map3 = nn.Linear(hidden_size, output_size)
         
     def forward(self, x):
-        x = F.softmax(self.map1(x))
+        x = F.tanh(self.map1(x))
+        x = F.leaky_relu(self.map2(x))
+        x = F.softmax(self.map3(x))
         return x
 
     
 class Outputter(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(Outputter, self).__init__()
-        self.map1 = nn.Linear(input_size, output_size)
+        self.map1 = nn.Linear(input_size, hidden_size)
+        self.map2 = nn.Linear(hidden_size, hidden_size)
+        self.map3 = nn.Linear(hidden_size, output_size)
         
     def forward(self, x):
-        x = F.elu(self.map1(x))
+        x = F.tanh(self.map1(x))
+        x = F.leaky_relu(self.map2(x))
+        x = F.elu(self.map3(x))
         return x
+
+
+class Enforcer(nn.Module):
+    def __init__(self, loss, similarity):
+        super(Enforcer, self).__init__()
+        self.loss = loss
+        self.similarity = similarity
+        
+    def forward(self, x, y):
+        return self.loss(self.similarity(x), self.similarity(y))
     
 
 # class Info(nn.Module):
@@ -240,43 +253,60 @@ def arangeIntervals(stop, step):
     return zip(numbers[:-1], numbers[1:])
 
 
+mynoise = Variable(gi_sampler(data_size, g_input_size))
+# mynoise = mydata  # autoencoder
+g_input_size = mynoise.shape[1]
+
 # d_sampler = get_distribution_sampler(data_mean, data_stddev)
 gi_sampler = get_generator_input_sampler()
-Gen = Generator(input_size=g_input_size, hidden_size=g_output_size, output_size=g_output_size)
-Out = Outputter(input_size=o_input_size, hidden_size=0, output_size=o_output_size)
-Clu = Clusterer(input_size=c_input_size, output_size=c_output_size)
+Gen = Generator(input_size=g_input_size, hidden_size=g_output_size, output_size=g_output_size, hd=hd)
+Out = Outputter(input_size=o_input_size, hidden_size=o_hidden_size, output_size=o_output_size)
+Clu = Clusterer(input_size=c_input_size, hidden_size=c_hidden_size,output_size=c_output_size)
+Enf = Enforcer(nn.MSELoss(), batch_cosine)
 
+tzero = Variable(torch.zeros(1))
 if gpu_mode:
     Gen = Gen.cuda()
     Gen.permuteTensor = Gen.permuteTensor.cuda()
+    Gen.hd = Gen.hd.cuda()
     Out = Out.cuda()
     Clu = Clu.cuda()
-
+    tzero = tzero.cuda()
+    
 # I = Info(input_size=d_hidden_size, hidden_size = d_hidden_size, output_size=c_size, hd=hd)
 criterion = nn.BCELoss()  # Binary cross entropy: http://pytorch.org/docs/nn.html#bceloss
 CE = nn.CrossEntropyLoss()
 MSE = nn.MSELoss()
 # d_optimizer = optim.Adam(D.parameters(), lr=d_learning_rate, betas=optim_betas, weight_decay=1e-6)
 g_optimizer = optim.Adam(Gen.parameters(), lr=g_learning_rate)
-o_optimizer = optim.RMSprop(itertools.chain(Out.parameters(),Gen.parameters()),
-                            lr=o_learning_rate)  # , weight_decay=1e-3)
-c_optimizer = optim.Adam(Clu.parameters(), lr=c_learning_rate, weight_decay=1e-6)
+o_optimizer = optim.Adam(itertools.chain(Out.parameters(),Gen.parameters()),
+                         lr=o_learning_rate)  # , weight_decay=1e-3)
+c_optimizer = optim.Adam(Clu.parameters(), lr=c_learning_rate, weight_decay=1e-4)
 # i_optimizer = optim.RMSprop(itertools.chain(G.parameters(), D.parameters()), lr=i_learning_rate)
-
-mynoise = Variable(gi_sampler(data_size, g_input_size))
 
 alpha = 1e-6
 
-    
+# if gpu_mode:
+#     mynoise = mynoise.cuda()
+#     mydata = mydata.cuda()
+
+pr_g_update = 1
+g_o_ratio = 1e-4
+pr_c_update = 1
+c_only = False
+
+# num_epochs=10000    
 for epoch in range(num_epochs):
-    epoch_losses = Variable(torch.zeros(3))
+    epoch_losses = Variable(torch.zeros(5))
     if gpu_mode:
         epoch_losses = epoch_losses.cuda()
-        
+
+    gcounter, ccounter = 0, 0
     g_epoch_indices = torch.LongTensor(np.random.choice(data_size, size=data_size))
     for i0, i1 in arangeIntervals(data_size, minibatch_size):
         g_minibatch_epoch_indices = \
             g_epoch_indices[i0:i1]
+            # Variable(g_epoch_indices[i0:i1]).cuda()
         # torch.LongTensor(np.random.choice(data_size, size=minibatch_size))
     
         Gen.zero_grad()
@@ -284,44 +314,108 @@ for epoch in range(num_epochs):
         Clu.zero_grad()
 
         # noise_data = mynoise[np.random.choice(data_size, size=minibatch_size),:]
-        noise_data = mynoise[g_minibatch_epoch_indices]
+        batch_noise = mynoise[g_minibatch_epoch_indices]
+        batch_mydata = mydata[g_minibatch_epoch_indices]
         if gpu_mode:
-            noise_data = noise_data.cuda()
-            mydata = mydata.cuda()
+            batch_noise = batch_noise.cuda()
+            batch_mydata = batch_mydata.cuda()
             g_minibatch_epoch_indices = g_minibatch_epoch_indices.cuda()
             
-        hidden = Gen(noise_data)
-        output = Out(hidden)
-        g_norms = torch.norm(hidden, 2, 1)
-        g_loss = torch.max(g_norms,1/(g_norms+1e-10)).mean()
-        g_loss += 1e-6*torch.pow(batch_cosine(hidden).sum(1)-0.5*minibatch_size,2).sum()  # spread out
-        g_loss.backward(retain_graph=True)
-        g_optimizer.step()
-        
-        o_loss = MSE(output, mydata[g_minibatch_epoch_indices])
-        o_loss.backward()
-        o_optimizer.step()
-        
-        chidden = Variable(hidden.data, requires_grad=False)
-        clusters = Clu(chidden)
-        c_loss = MSE(
-            batch_cosine(torch.sqrt(clusters), normalize=False),
-            batch_cosine(chidden))
-        c_loss.backward()
-        c_optimizer.step()
+        hidden = Gen(batch_noise)
 
-        epoch_losses += torch.cat((g_loss, o_loss, c_loss))
+        if np.random.uniform() < pr_g_update:
+            g_norms = torch.norm(hidden, 2, 1)
+            g_loss = torch.max(g_norms,1-torch.log(g_norms+1e-10)).mean()
+            # s_loss = 1e-1*torch.pow(batch_cosine(hidden).mean() - 0.5,2)  # spread out (batch)
+            s_loss = tzero
+            # g_loss += s_loss
+            # g_loss.backward(retain_graph=True)  # incorporate into o_optimizer insead.
+            # g_optimizer.step()
+            gcounter += 1
+        else:
+            g_loss, s_loss = tzero, tzero
+
+        if not c_only:
+            output = Out(hidden)
+            o_loss = MSE(output, batch_mydata)
+            e_loss = 1e-1 * Enf(hidden, batch_mydata)  # Enforce H and O similarity
+            o_loss += e_loss
+            o_loss += g_loss * g_o_ratio
+            o_loss.backward()
+            o_optimizer.step()
+
+        if np.random.uniform() < pr_c_update:
+            chidden = Variable(hidden.data, requires_grad=False)
+            clusters = Clu(chidden)
+            c_loss = MSE(
+                batch_cosine(torch.sqrt(clusters+1e-10), normalize=False),
+                batch_cosine(chidden))
+            c_e_loss = ((clusters*torch.log(clusters+1e-10)).mean()+1./minibatch_size).pow(2)  # Match on desired entropy
+            c_loss += c_e_loss
+            c_loss.backward()
+            c_optimizer.step()
+            ccounter += 1
+        else:
+            c_loss = tzero
+
+        epoch_losses += torch.cat((g_loss, s_loss, o_loss, e_loss, c_loss))
 
     if epoch % print_interval == 0:
-        print("%s: H: %6.3f;  O: %6.3f; C: %6.3f" % (epoch,
-                                                     epoch_losses.cpu().data.numpy()[0],
-                                                     epoch_losses.cpu().data.numpy()[1],
-                                                     epoch_losses.cpu().data.numpy()[2]))
+        el = epoch_losses.cpu().data.numpy()
+        el *= 1*minibatch_size/data_size*\
+              np.array([1.*data_size/minibatch_size/gcounter,
+                        1.*data_size/minibatch_size/gcounter,
+                        1,
+                        1,
+                        1.*data_size/minibatch_size/ccounter])
+        print("%s: [H: %6.4f;  s: %6.4f]; [O: %6.4f; e: %6.4f]; C: %6.4f" %
+              (epoch, el[0], el[1], el[2], el[3], el[4]))
 
-
-assignments = np.argmax(Clu(Gen(mynoise.cuda())).cpu().data.numpy(), axis=1)
+assignments = np.zeros(mynoise.shape[0])
+for i0, i1 in arangeIntervals(data_size, 100):
+    assignments[i0:i1] = np.argmax(Clu(Gen(mynoise[i0:i1].cuda())).cpu().data.numpy(), axis=1)
     # for i0, i1 in arangeIntervals(data_size, minibatch_size):
-print(np.bincount(assignments))
+print(np.bincount(assignments.astype(int)))
+print(batch_cosine(mydata[:100]))
+print(batch_cosine(Out(Gen(mynoise[:100].cuda()))))
+
+assignments2 = np.argmax(np.log(Clu(Gen(mynoise.cuda())).cpu().data.numpy()) -
+                         np.log(np.mean(Clu(Gen(mynoise.cuda())).cpu().data.numpy(),0,
+                                        keepdims=True)), axis=1)
+print(np.bincount(assignments2))
+
+# mydatadf = pd.read_csv(clustercsv)
+# pd.DataFrame({'drug': mydatadf.drop('npi',1).columns,
+#               'cluster':assignments}).to_csv('180416drgclusters.csv')
+npidf = pd.DataFrame({'npi':mydatadf['npi'],
+                      'cluster':assignments})
+npidf.to_csv('180416clusters.csv')
+
+npissaved = pd.read_csv('180415clusters.csv')
+image_w_cluster = mydata.cpu().data.numpy()[:,np.argsort(npissaved.cluster)]
+# image_w_cluster = mydata.cpu().data.numpy()[:,np.random.permutation(mydata.shape[1])]
+image_ordered = image_w_cluster[np.argsort(assignments),:]
+plt.imshow(image_ordered, aspect='auto')
+plt.show()
+
+plt.subplot(121)
+plt.imshow(mydata[:1000].cpu().data.numpy())
+plt.clim(0,20)
+plt.subplot(122)
+plt.imshow(Out(Gen(mynoise[:1000].cuda())).cpu().data.numpy())
+plt.clim(0,20)
+# plt.colorbar()
+plt.show()
+
+# Get hidden:
+hiddenVectors = np.zeros((mynoise.shape[0], hidden_size))
+for i0, i1 in arangeIntervals(data_size, 100):
+    hiddenVectors[i0:i1] = Gen(mynoise[i0:i1].cuda()).cpu().data.numpy()
+
+npihiddendf = pd.DataFrame(hiddenVectors)
+npihiddendf['npi'] = mydatadf['npi']
+npihiddendf.to_csv('180416npihidden.csv')
+
 
 print('A few ', np.argmax(softs, axis=1)[:10])
 print(np.bincount(np.argmax(softs, axis=1)))
@@ -336,17 +430,6 @@ print(K.eval((K.dot(K.variable(h[:5]), K.transpose(K.variable(h[:5]))))) - \
 # How about by the ratio of p(_)/mean(p(_))?
 assignments2 = np.argmax(np.log(softs) - np.log(np.mean(softs,0, keepdims=True)), axis=1)
 print(np.bincount(assignments2))
-
-mydatadf = pd.read_csv(clustercsv)
-npidf = pd.DataFrame({'npi':mydatadf['npi'],
-                      'cluster':assignments})
-npidf.to_csv('180412clusters.csv')
-
-image_w_cluster = mydata.cpu().data.numpy()[:,np.random.permutation(mydata.shape[1])]
-image_ordered = image_w_cluster[np.argsort(assignments),:]
-plt.imshow(image_ordered, aspect='auto')
-plt.show()
-
 
 
 #### OLD ####
